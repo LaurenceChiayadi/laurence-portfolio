039588a038621bcf00558c57ac9c6008341ab483
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2025-04-04 10:41:06 +0700

    Make background className reusable

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,17 @@ import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import Projects from "./components/Projects";
 import Tech from "./components/Tech";
-import { useTheme, ThemeEnum } from "./contexts/ThemeContext";
+import { useTheme } from "./contexts/ThemeContext";
+import { getBackgroundClass } from "./utilities/ThemeUtilities";
 
 function App() {
   const { theme } = useTheme();
   return (
     <>
       <div
-        className={
-          theme === ThemeEnum.Light
-            ? "fixed -z 10 min-h-screen w-full bg-white bg-[radial-gradient(#d3d3d3_1px,transparent_1px)] [background-size:20px_20px]"
-            : "fixed -z 10 min-h-screen w-full bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"
-        }
+        className={`fixed -z-10 min-h-screen w-full ${getBackgroundClass(
+          theme
+        )}`}
       ></div>
       <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
         <NavBar />
