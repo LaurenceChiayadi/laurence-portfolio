ace4fa87e810bd8403eebc4ffcac45bb0f41dc6b
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2025-04-08 22:05:33 +0700

    change image size due to safari making about image warped

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,10 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
-import SelfPhoto from "../assets/self-photo.jpg";
-import Blob from "./global/Animation/Blob";
-import Wrapper from "./global/Wrapper";
-import { H3, H6 } from "./global/Typography/Typographies";
-import AnimateOnSeen from "./global/Animation/AnimateOnSeen";
+import SelfPhoto from '../assets/self-photo.jpg';
+import Blob from './global/Animation/Blob';
+import Wrapper from './global/Wrapper';
+import { H3, H6 } from './global/Typography/Typographies';
+import AnimateOnSeen from './global/Animation/AnimateOnSeen';
 
 const About = () => {
   const { t } = useTranslation();
@@ -17,13 +17,13 @@ const About = () => {
           <img
             src={SelfPhoto}
             alt="blob"
-            className="w-full h-full object-cover"
-            style={{ clipPath: "url(#blobClip)" }}
+            className="h-100 object-cover"
+            style={{ clipPath: 'url(#blobClip)' }}
           />
         </AnimateOnSeen>
         <div className="flex flex-col items-start gap-4  md:w-42/100">
           <AnimateOnSeen>
-            <H3>{t("about.title")}</H3>
+            <H3>{t('about.title')}</H3>
           </AnimateOnSeen>
           <AnimateOnSeen>
             <H6 i18nKey="about.intro" />
