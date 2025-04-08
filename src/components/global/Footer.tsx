e4419e01daba5eac30c9af59e9b2e3138829fced
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2025-04-08 16:28:40 +0700

    Prevent overflow from footer

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -5,9 +5,12 @@ import { Subtitle } from "./Typography/Typographies";
 const Footer = () => {
   const { t } = useTranslation();
   return (
-    <div className="w-full flex flex-col text-center pb-4">
-      <Subtitle>{t("footer.top")}</Subtitle>
-      <Subtitle i18nKey="footer.bottom" />
+    <div className="w-full flex flex-col items-center text-center pb-4">
+      <Subtitle className="max-w-90 md:max-w-100">{t("footer.top")}</Subtitle>
+      <Subtitle
+        className="max-w-80 text-center md:max-w-150"
+        i18nKey="footer.bottom"
+      />
     </div>
   );
 };
