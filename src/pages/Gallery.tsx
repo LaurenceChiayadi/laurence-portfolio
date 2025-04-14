4c5e8d92ae0f21331e0308fa0ab029d3af653603
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2025-04-14 16:14:06 +0700

    Add image enlarge

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
-
 import CameraImages from "../constants/CameraImages";
 import { useEffect, useState } from "react";
 import { getImageOrientation } from "../utilities/Functions";
@@ -9,6 +8,7 @@ const Gallery = () => {
   const navigate = useNavigate();
   const [sortedImages, setSortedImages] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
   useEffect(() => {
     const sortImagesByOrientation = async () => {
@@ -36,6 +36,21 @@ const Gallery = () => {
     sortImagesByOrientation();
   }, []);
 
+  const handleImageClick = (image: string) => {
+    setSelectedImage(image);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
+
+  const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    // Check if the click target is the background overlay (not the image)
+    if (e.target === e.currentTarget) {
+      handleCloseModal();
+    }
+  };
+
   return (
     <div className="py-8 md:py-24 px-4 sm:px-8">
       <div className="flex flex-col-reverse md:flex-row justify-between md:items-center">
@@ -53,6 +68,7 @@ const Gallery = () => {
           ← Go Back
         </button>
       </div>
+
       <div className="backdrop-blur-md bg-white/10 border border-white/20 rounded-2xl shadow-xl p-4">
         {isLoading ? (
           <div className="flex justify-center py-12">
@@ -72,12 +88,42 @@ const Gallery = () => {
                   duration: 0.2,
                   delay: Math.min(index * 0.05, 0.5),
                 }}
-                className="w-full transition-transform duration-300 hover:scale-105 hover:brightness-110 rounded-lg shadow-md"
+                className="w-full transition-transform duration-300 hover:scale-105 hover:brightness-110 rounded-lg shadow-md cursor-pointer"
+                onClick={() => handleImageClick(image)}
               />
             ))}
           </div>
         )}
       </div>
+
+      {selectedImage && (
+        <motion.div
+          className="fixed inset-0 bg-black/60 flex justify-center items-center z-50"
+          onClick={handleOverlayClick} // Listen for click on overlay
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+        >
+          <div className="relative">
+            <button
+              onClick={handleCloseModal}
+              className="absolute top-0 right-0 p-4 text-white text-2xl"
+            >
+              &times;
+            </button>
+            <motion.img
+              src={selectedImage}
+              alt="Enlarged"
+              className="max-w-full max-h-screen object-contain"
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              exit={{ scale: 0 }}
+              transition={{ duration: 0.5, ease: "easeOut" }}
+            />
+          </div>
+        </motion.div>
+      )}
     </div>
   );
 };
