45129ad2a54238f49735c716736bc609c005d4bc
Laurence Chiayadi <laurence.tjahyadi@gmail.com>
2025-04-17 14:27:42 +0700

    Add cursor pointer to gradient button

--- src/components/global/GradientButton.tsx
+++ src/components/global/GradientButton.tsx
@@ -8,7 +8,7 @@ const GradientButton = (props: GradientButtonProps) => {
   return (
     <button
       onClick={props.onClick}
-      className={`bg-gradient-to-r from-blue-500 to-pink-500 rounded-lg px-3 py-1.5 hover:scale-120 duration-300 text-xl ${props.className}`}
+      className={`cursor-pointer bg-gradient-to-r from-blue-500 to-pink-500 rounded-lg px-3 py-1.5 hover:scale-120 duration-300 text-xl ${props.className}`}
     >
       <span className="flex flex-row items-center gap-2 font-semibold text-gray-800">
         {props.children}
